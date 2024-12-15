1cef9c92bdcb4bbbedc6dd2b5f31186f35864ff8
Daniel Jeon <45351514+Daniel-Jeon@users.noreply.github.com>
2024-12-15 22:15:15 +0900

    do not use pug anymore
    hmm..

--- src/db.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-// mongodb와 nodejs를 연결하기 위해 mongoose를 사용
-mongoose.connect(process.env.DB_URL);
-const db = mongoose.connection;
-
-// https://mongoosejs.com/docs/connections.html#connection-events
-db.once("on", (error) => console.log(error));
-db.once("open", () => console.log("DB Connected"));
