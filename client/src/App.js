3e20b02e29361b2337860379c6836ca4abf0fb7e
Daniel Jeon <45351514+Daniel-Jeon@users.noreply.github.com>
2025-01-13 23:27:03 +0900

    ok

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,7 @@ import Search from "./videos/Search";
 
 const App = () => {
   const { user } = useContext(UserContext);
+  console.log("API URL:", process.env.REACT_APP_API_URL);
   return (
     <BrowserRouter>
       <Header user={user} />
